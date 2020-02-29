1dfd634d0112297f4dbe2be6a35471f3a64313f8
sozysozbot <sozysozbot@gmail.com>
2020-02-29 16:48:34 +0900

    npm run format

--- src/main_entry.ts
+++ src/main_entry.ts
@@ -1,6 +1,6 @@
 const {
   calculate_hands_and_score_from_pieces,
-  hand_to_score
+  hand_to_score,
 } = require("exports-loader?calculate_hands_and_score_from_pieces,hand_to_score!./lib/cerke_calculate_hands/calculate_hand");
 (window as any).calculate_hands_and_score_from_pieces = calculate_hands_and_score_from_pieces;
 (window as any).hand_to_score = hand_to_score;
